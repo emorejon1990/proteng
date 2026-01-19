9c9c6d816fd14c51613b8a730a06243b48c9a77c
emorejon1990 <emorejon1990@gmail.com>
2026-01-19 12:40:40 -0500

    ...

--- resources/views/filament/pages/quality.blade.php
+++ resources/views/filament/pages/quality.blade.php
@@ -48,10 +48,37 @@
         x-data="{}"
         x-load-js="[@js(\Filament\Support\Facades\FilamentAsset::getScriptSrc('weightscale'))]">
         @foreach ($this->workOrders as $order)
-            <div class="p-4 bg-white rounded shadow card">
+            <div class="p-4 bg-white rounded shadow card" x-data="{ open: false }">
                 <h2 class="text-xl font-bold mb-2">
-                    {{ $order->name }} ({{ $order->asset_name }})
+                    {{ $order->name }}
+                    (<button type="button"
+                        class="underline decoration-dotted text-left"
+                        @click="open = true">
+                        {{ $order->asset_name }}
+                    </button>)
                 </h2>
+
+                <div
+                    x-cloak
+                    x-show="open"
+                    class="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
+                    @click.self="open = false"
+                >
+                    <div class="bg-white rounded shadow-lg max-w-2xl w-full p-6 overflow-y-auto" style="max-height: 60%;">
+                        <div class="flex items-start justify-between mb-4">
+                            <h3 class="text-lg font-semibold">
+                                {{ $order->asset_name }}
+                            </h3>
+                            <button type="button" class="text-gray-500" @click="open = false">
+                                &#10005;
+                            </button>
+                        </div>
+                        <div class="text-sm text-gray-700">
+                            {!! optional($order->asset)->description !!}
+                        </div>
+                    </div>
+                </div>
+
                 <livewire:work-order-quality-editor :work-order="$order" :wire:key="'wo-'.$order->id" />
             </div>
         @endforeach
